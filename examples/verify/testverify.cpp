10e8b36445fbc3ab7167778439ed18f364394346
petermcLBL <PWMcCorquodale@lbl.gov>
2020-12-07 18:13:44 -0800

    This works now, with FLIP on dimensions of MDDFT or IMDDFT.

--- examples/verify/testverify.cpp
+++ examples/verify/testverify.cpp
@@ -1,8 +1,10 @@
 
 #include <cmath> // Without this, abs returns zero!
 #include <random>
-#include "verify_mddft.fftx.codegen.hpp"
-#include "verify_imddft.fftx.codegen.hpp"
+#include "mddft2.fftx.codegen.hpp"
+#include "imddft2.fftx.codegen.hpp"
+#include "mddft3.fftx.codegen.hpp"
+#include "imddft3.fftx.codegen.hpp"
 #include "verify.h"
 
 // using namespace fftx;
@@ -11,7 +13,13 @@ std::mt19937 generator;
 std::uniform_real_distribution<double> unifReal;
 std::uniform_int_distribution<int> unifInt[3];
 
-fftx::point_t<3> unifPoint()
+fftx::point_t<2> unifPoint2()
+{
+  return fftx::point_t<2>({{unifInt[0](generator),
+                            unifInt[1](generator)}});
+}
+
+fftx::point_t<3> unifPoint3()
 {
   return fftx::point_t<3>({{unifInt[0](generator),
                             unifInt[1](generator),
@@ -25,36 +33,39 @@ std::complex<double> unifComplex()
                               unifReal(generator));
 }
 
+template<int DIM>
 std::complex<double> unifComplexValPoint(std::complex<double>& a_v,
-                                         fftx::point_t<3> a_pt)
+                                         fftx::point_t<DIM> a_pt)
 {
   return unifComplex();
 }
 
-void unifComplexArray(fftx::array_t<3, std::complex<double>> a_arr)
+template<int DIM>
+void unifComplexArray(fftx::array_t<DIM, std::complex<double>> a_arr)
 {
   auto arrPtr = a_arr.m_data.local();
-  auto domain = a_arr.m_domain;
-  size_t npts = domain.size();
+  auto dom = a_arr.m_domain;
+  size_t npts = dom.size();
   for (size_t ind = 0; ind < npts; ind++)
     {
       arrPtr[ind] = unifComplex();
     }
 }
 
-void sumScaledArrays(fftx::array_t<3, std::complex<double>> a_sum,
-                     const fftx::array_t<3, std::complex<double>> a_arr1,
+template<int DIM>
+void sumScaledArrays(fftx::array_t<DIM, std::complex<double>> a_sum,
+                     const fftx::array_t<DIM, std::complex<double>> a_arr1,
                      std::complex<double> a_scale1,
-                     const fftx::array_t<3, std::complex<double>> a_arr2,
+                     const fftx::array_t<DIM, std::complex<double>> a_arr2,
                      std::complex<double> a_scale2)
 {
-  auto domain = a_sum.m_domain;
-  assert(domain == a_arr1.m_domain);
-  assert(domain == a_arr2.m_domain);
+  auto dom = a_sum.m_domain;
+  assert(dom == a_arr1.m_domain);
+  assert(dom == a_arr2.m_domain);
   auto sumPtr = a_sum.m_data.local();
   auto arr1Ptr = a_arr1.m_data.local();
   auto arr2Ptr = a_arr2.m_data.local();
-  size_t npts = domain.size();
+  size_t npts = dom.size();
   for (size_t ind = 0; ind < npts; ind++)
     {
       sumPtr[ind] =
@@ -63,19 +74,21 @@ void sumScaledArrays(fftx::array_t<3, std::complex<double>> a_sum,
     }
 }
 
-void setConstant(fftx::array_t<3, std::complex<double>>& a_arr,
+template<int DIM>
+void setConstant(fftx::array_t<DIM, std::complex<double>>& a_arr,
                  std::complex<double> a_val)
 {
-  forall([a_val](std::complex<double>(&v), const fftx::point_t<3>& p)
+  forall([a_val](std::complex<double>(&v), const fftx::point_t<DIM>& p)
          {
            v = a_val;
          }, a_arr);
 }
 
-void setUnitImpulse(fftx::array_t<3, std::complex<double>>& a_arr,
-                    const fftx::point_t<3>& a_fixed)
+template<int DIM>
+void setUnitImpulse(fftx::array_t<DIM, std::complex<double>>& a_arr,
+                    const fftx::point_t<DIM>& a_fixed)
 {
-  forall([a_fixed](std::complex<double>(&v), const fftx::point_t<3>& p)
+  forall([a_fixed](std::complex<double>(&v), const fftx::point_t<DIM>& p)
          {
            if (p == a_fixed)
              {
@@ -88,34 +101,37 @@ void setUnitImpulse(fftx::array_t<3, std::complex<double>>& a_arr,
          }, a_arr);
 }
 
-void setProductWaves(fftx::array_t<3, std::complex<double>>& a_arr,
-                     const fftx::point_t<3>& a_fixed)
+template<int DIM>
+void setProductWaves(fftx::array_t<DIM, std::complex<double>>& a_arr,
+                     const fftx::point_t<DIM>& a_fixed)
 {
-  fftx::point_t<3> extent = a_arr.m_domain.extents();
-  fftx::point_t<3> cornerLo = a_arr.m_domain.lo;
-  std::complex<double> omega[3];
-  for (int d = 0; d < 3; d++)
+  fftx::point_t<DIM> extent = a_arr.m_domain.extents();
+  fftx::point_t<DIM> cornerLo = a_arr.m_domain.lo;
+  std::complex<double> omega[DIM];
+  for (int d = 0; d < DIM; d++)
     {
       double th = (-2 * (a_fixed[d] - cornerLo[d])) * M_PI / (extent[d] * 1.);
       omega[d] = std::complex<double>(cos(th), sin(th));
-      std::cout << d << " th = " << th << " omega = " << omega[d] << std::endl;
     }
-  forall([omega, cornerLo](std::complex<double>(&v), const fftx::point_t<3>& p)
+  forall([omega, cornerLo](std::complex<double>(&v), const fftx::point_t<DIM>& p)
          {
-           v = pow(omega[0], p[0] - cornerLo[0]) *
-               pow(omega[1], p[1] - cornerLo[1]) *
-               pow(omega[2], p[2] - cornerLo[2]);
+           v = std::complex<double>(1., 0.);
+           for (int d = 0; d < DIM; d++)
+             {
+               v *= pow(omega[d], p[d] - cornerLo[d]);
+             }
          }, a_arr);
 }
 
-double absMaxDiffArray(fftx::array_t<3, std::complex<double>>& a_arr1,
-                       fftx::array_t<3, std::complex<double>>& a_arr2)
+template<int DIM>
+double absMaxDiffArray(fftx::array_t<DIM, std::complex<double>>& a_arr1,
+                       fftx::array_t<DIM, std::complex<double>>& a_arr2)
 {
-  auto domain = a_arr1.m_domain;
-  assert(domain == a_arr2.m_domain);
+  auto dom = a_arr1.m_domain;
+  assert(dom == a_arr2.m_domain);
   auto arr1Ptr = a_arr1.m_data.local();
   auto arr2Ptr = a_arr2.m_data.local();
-  size_t npts = domain.size();
+  size_t npts = dom.size();
   double absDiffMax = 0.;
   for (size_t ind = 0; ind < npts; ind++)
     {
@@ -130,7 +146,6 @@ double absMaxDiffArray(fftx::array_t<3, std::complex<double>>& a_arr1,
 int main(int argc, char* argv[])
 {
   printf("%s: Entered test program\n", argv[0]);
-  std::cout << "domain = " << verify::domain << std::endl;
   int rounds = 20;
   if (argc > 1)
     {
@@ -138,13 +153,18 @@ int main(int argc, char* argv[])
     }
   printf("Number of rounds: %d\n", rounds);
 	
-  printf("Call verify_mddft::init()\n");
-  verify_mddft::init();
-  printf("Call verify_imddft::init()\n");
-  verify_imddft::init();
+  printf("Call mddft2::init()\n");
+  mddft2::init();
+  printf("Call imddft2::init()\n");
+  imddft2::init();
 
-  fftx::array_t<3, std::complex<double>> input(verify::domain);
-  fftx::array_t<3, std::complex<double>> output(verify::domain);
+  printf("Call mddft3::init()\n");
+  mddft3::init();
+  printf("Call imddft3::init()\n");
+  imddft3::init();
+
+  // fftx::array_t<3, std::complex<double>> input(verify::domain3);
+  // fftx::array_t<3, std::complex<double>> output(verify::domain3);
 
   /*
     Set up random number generator.
@@ -154,22 +174,27 @@ int main(int argc, char* argv[])
   unifReal = std::uniform_real_distribution<double>(-0.5, 0.5);
   for (int d = 0; d < 3; d++)
     {
-      unifInt[d] = std::uniform_int_distribution<int>(verify::domain.lo[d],
-                                                      verify::domain.hi[d]);
+      unifInt[d] = std::uniform_int_distribution<int>(verify::domain3.lo[d],
+                                                      verify::domain3.hi[d]);
     }
   
+
+  std::cout << "*******************************************" << std::endl;
+  std::cout << "2D domain = " << verify::domain2 << std::endl;
+  std::cout << "*******************************************" << std::endl;
+
   /*
-    Test 1: Check linearity.
+    2D test 1: Check linearity.
    */
   {
-    fftx::array_t<3, std::complex<double>> inA(verify::domain);
-    fftx::array_t<3, std::complex<double>> inB(verify::domain);
-    fftx::array_t<3, std::complex<double>> LCin(verify::domain);
+    fftx::array_t<2, std::complex<double>> inA(verify::domain2);
+    fftx::array_t<2, std::complex<double>> inB(verify::domain2);
+    fftx::array_t<2, std::complex<double>> LCin(verify::domain2);
 
-    fftx::array_t<3, std::complex<double>> outA(verify::domain);
-    fftx::array_t<3, std::complex<double>> outB(verify::domain);
-    fftx::array_t<3, std::complex<double>> LCout(verify::domain);
-    fftx::array_t<3, std::complex<double>> outLCin(verify::domain);
+    fftx::array_t<2, std::complex<double>> outA(verify::domain2);
+    fftx::array_t<2, std::complex<double>> outB(verify::domain2);
+    fftx::array_t<2, std::complex<double>> LCout(verify::domain2);
+    fftx::array_t<2, std::complex<double>> outLCin(verify::domain2);
     for (int itn = 0; itn < rounds; itn++)
       {
         std::complex<double> alpha = unifComplex();
@@ -182,79 +207,153 @@ int main(int argc, char* argv[])
         sumScaledArrays(LCin, inA, alpha, inB, beta);
         
         { // Test MDDFT.
-          verify_mddft::transform(inA, outA);
-          verify_mddft::transform(inB, outB);
+          mddft2::transform(inA, outA);
+          mddft2::transform(inB, outB);
           sumScaledArrays(LCout, outA, alpha, outB, beta);
-          verify_mddft::transform(LCin, outLCin);
+          mddft2::transform(LCin, outLCin);
 
           double dd = absMaxDiffArray(outLCin, LCout);
-          printf("MDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
+          printf("2D MDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
         }
 
         { // Test IMDDFT.
-          verify_imddft::transform(inA, outA);
-          verify_imddft::transform(inB, outB);
+          imddft2::transform(inA, outA);
+          imddft2::transform(inB, outB);
           sumScaledArrays(LCout, outA, alpha, outB, beta);
-          verify_imddft::transform(LCin, outLCin);
+          imddft2::transform(LCin, outLCin);
 
           double dd = absMaxDiffArray(outLCin, LCout);
-          printf("IMDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
+          printf("2D IMDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
         }
       }
     }
 
   /*
-    Test 2: Check that unit impulse is transformed properly.
+    2D test 2: Check that unit impulse is transformed properly.
   */
   {
-    fftx::array_t<3, std::complex<double>> inImpulse(verify::domain);
-    fftx::array_t<3, std::complex<double>> outImpulse(verify::domain);
-    fftx::array_t<3, std::complex<double>> outCheck(verify::domain);
+    fftx::array_t<2, std::complex<double>> inImpulse(verify::domain2);
+    fftx::array_t<2, std::complex<double>> outImpulse(verify::domain2);
+    fftx::array_t<2, std::complex<double>> outCheck(verify::domain2);
 
     // Unit impulse at low corner.
-    setUnitImpulse(inImpulse, verify::domain.lo);
+    setUnitImpulse(inImpulse, verify::domain2.lo);
     std::complex<double> one = std::complex<double>(1., 0.);
     setConstant(outCheck, one);
     {
-      verify_mddft::transform(inImpulse, outImpulse);
+      mddft2::transform(inImpulse, outImpulse);
       double dd = absMaxDiffArray(outImpulse, outCheck);
-      printf("Unit impulse low corner test on MDDFT max abs diff = %e\n", dd);
+      printf("2D unit impulse low corner test on MDDFT max abs diff = %e\n", dd);
     }
     {
-      verify_imddft::transform(inImpulse, outImpulse);
+      imddft2::transform(inImpulse, outImpulse);
       double dd = absMaxDiffArray(outImpulse, outCheck);
-      printf("Unit impulse low corner test on IMDDFT max abs diff = %e\n", dd);
+      printf("2D unit impulse low corner test on IMDDFT max abs diff = %e\n", dd);
     }
 
-    for (int itn = 0; itn < verify::nx; itn++) // FIXME: itn < rounds
+    for (int itn = 0; itn < rounds; itn++)
+        {
+          fftx::point_t<2> rpoint = unifPoint2();
+          setUnitImpulse(inImpulse, rpoint);
+          mddft2::transform(inImpulse, outImpulse);
+          setProductWaves(outCheck, rpoint);
+          double dd = absMaxDiffArray(outImpulse, outCheck);
+          std::cout << "2D unit impulse test of MDDFT at " << rpoint
+                    << " max abs diff = " << dd << std::endl;
+        }
+  }
+  
+
+
+  std::cout << "*******************************************" << std::endl;
+  std::cout << "2D domain = " << verify::domain3 << std::endl;
+  std::cout << "*******************************************" << std::endl;
+
+  /*
+    3D test 1: Check linearity.
+   */
+  {
+    fftx::array_t<3, std::complex<double>> inA(verify::domain3);
+    fftx::array_t<3, std::complex<double>> inB(verify::domain3);
+    fftx::array_t<3, std::complex<double>> LCin(verify::domain3);
+
+    fftx::array_t<3, std::complex<double>> outA(verify::domain3);
+    fftx::array_t<3, std::complex<double>> outB(verify::domain3);
+    fftx::array_t<3, std::complex<double>> LCout(verify::domain3);
+    fftx::array_t<3, std::complex<double>> outLCin(verify::domain3);
+    for (int itn = 0; itn < rounds; itn++)
       {
-        fftx::point_t<3> rpoint = fftx::point_t<3>({{itn+1, 1, 1}}); // FIXME: was unifPoint()
+        std::complex<double> alpha = unifComplex();
+        std::complex<double> beta = unifComplex();
+        // These do not seem to have any effect.  Sorry.
+        // forall(unifComplexValPoint, inA);
+        // forall(unifComplexValPoint, inB);
+        unifComplexArray(inA);
+        unifComplexArray(inB);
+        sumScaledArrays(LCin, inA, alpha, inB, beta);
+        
+        { // Test MDDFT.
+          mddft3::transform(inA, outA);
+          mddft3::transform(inB, outB);
+          sumScaledArrays(LCout, outA, alpha, outB, beta);
+          mddft3::transform(LCin, outLCin);
+
+          double dd = absMaxDiffArray(outLCin, LCout);
+          printf("3D MDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
+        }
+
+        { // Test IMDDFT.
+          imddft3::transform(inA, outA);
+          imddft3::transform(inB, outB);
+          sumScaledArrays(LCout, outA, alpha, outB, beta);
+          imddft3::transform(LCin, outLCin);
+
+          double dd = absMaxDiffArray(outLCin, LCout);
+          printf("3D IMDDFT linearity test %d max abs diff = %e\n", itn+1, dd);
+        }
+      }
+    }
+
+  /*
+    3D test 2: Check that unit impulse is transformed properly.
+  */
+  {
+    fftx::array_t<3, std::complex<double>> inImpulse(verify::domain3);
+    fftx::array_t<3, std::complex<double>> outImpulse(verify::domain3);
+    fftx::array_t<3, std::complex<double>> outCheck(verify::domain3);
+
+    // Unit impulse at low corner.
+    setUnitImpulse(inImpulse, verify::domain3.lo);
+    std::complex<double> one = std::complex<double>(1., 0.);
+    setConstant(outCheck, one);
+    {
+      mddft3::transform(inImpulse, outImpulse);
+      double dd = absMaxDiffArray(outImpulse, outCheck);
+      printf("3D unit impulse low corner test on MDDFT max abs diff = %e\n", dd);
+    }
+    {
+      imddft3::transform(inImpulse, outImpulse);
+      double dd = absMaxDiffArray(outImpulse, outCheck);
+      printf("3D unit impulse low corner test on IMDDFT max abs diff = %e\n", dd);
+    }
+
+    for (int itn = 0; itn < rounds; itn++)
+      {
+        fftx::point_t<3> rpoint = unifPoint3();
         setUnitImpulse(inImpulse, rpoint);
-        verify_mddft::transform(inImpulse, outImpulse);
+        mddft3::transform(inImpulse, outImpulse);
         setProductWaves(outCheck, rpoint);
         double dd = absMaxDiffArray(outImpulse, outCheck);
-        std::cout << "Unit impulse test of MDDFT at " << rpoint
+        std::cout << "3D unit impulse test of MDDFT at " << rpoint
                   << " max abs diff = " << dd << std::endl;
-        auto outImpulsePtr = outImpulse.m_data.local();
-        auto outCheckPtr = outCheck.m_data.local();
-        for (int i = verify::domain.lo[0]; i <= verify::domain.hi[0]; i++)
-          for (int j = verify::domain.lo[1]; j <= verify::domain.hi[1]; j++)
-            for (int k = verify::domain.lo[2]; k <= verify::domain.hi[2]; k++)
-              {
-                fftx::point_t<3> pt = fftx::point_t<3>({{i, j, k}});
-                size_t pos = fftx::positionInBox(pt, verify::domain);
-                std::complex<double> outImpulseHere = outImpulsePtr[pos];
-                std::complex<double> outCheckHere = outCheckPtr[pos];
-                std::cout << pt
-                          << " outImpulse=" << outImpulseHere
-                          << " outCheck=" << outCheckHere
-                          << std::endl;
-              }
       }
   }
   
-  verify_mddft::destroy();
-  verify_imddft::destroy();
+  mddft2::destroy();
+  imddft2::destroy();
+
+  mddft3::destroy();
+  imddft3::destroy();
 
   printf("%s: All done, exiting\n", argv[0]);
   return 0;
--- examples/verify/verify.h
+++ examples/verify/verify.h
@@ -1,11 +1,28 @@
 using namespace fftx;
 
+template<int DIM> point_t<DIM> FLIP(point_t<DIM> p)
+{
+  point_t<DIM> pflip;
+  for (int d = 0; d < DIM; d++)
+    {
+      pflip[d] = p[DIM-1 - d];
+    }
+  return pflip;
+}
+
 namespace verify
 {
-  const int nx=3;
-  const int ny=4;
-  const int nz=5;
+  const int nx = 24;
+  const int ny = 32;
+  const int nz = 40;
 
-  box_t<3> empty(point_t<3>({{1,1,1}}), point_t<3>({{0,0,0}}));
-  box_t<3> domain(point_t<3>({{1,1,1}}), point_t<3>({{nx,ny,nz}}));
+  box_t<2> empty2(point_t<2>({{1, 1}}),
+                  point_t<2>({{0, 0}}));
+  box_t<2> domain2(point_t<2>({{1, 1}}),
+                   point_t<2>({{nx, ny}}));
+
+  box_t<3> empty3(point_t<3>({{1, 1, 1}}),
+                  point_t<3>({{0, 0, 0}}));
+  box_t<3> domain3(point_t<3>({{1, 1, 1}}),
+                   point_t<3>({{nx, ny, nz}}));
 }
