c6af639665a148bebfc5d22a959052fee2abf1ea
petermcLBL <PWMcCorquodale@lbl.gov>
2021-04-09 18:06:08 -0700

    Using boxes that are easier to understand.

--- examples/hockney/hockney.h
+++ examples/hockney/hockney.h
@@ -10,18 +10,12 @@ namespace hockney
   //    sbox                    dbox
 
   // n >= ns + nd
-  const int ns =  3; // 8; // 33;
-  const int nd =  5; // 24; // 96;
-  const int n  = 10; // 33; // 130;
-  const int noutShift = n + ns;
+  const int ns = 45;
+  const int nd = 31;
+  const int n  = 80;
 
-  // box_t<3> sbox({{0, 0, 0}}, {{ns-1, ns-1, ns-1}});
-  box_t<3> sbox({{0, 0, 0}}, {{2*ns, 2*ns, 2*ns}});
-  // box_t<3> dbox({{n-nd, n-nd, n-nd}}, {{n-1, n-1, n-1}});
-  box_t<3> dbox({{noutShift-nd, noutShift-nd, noutShift-nd}},
-                {{noutShift+nd, noutShift+nd, noutShift+nd}});
-  // box_t<3> rdomain({{0, 0, 0}}, {{n-1, n-1, n-1}});
-  box_t<3> rdomain({{0, 0, 0}}, {{2*n-1, 2*n-1, 2*n-1}});
-  // box_t<3> freq({{0, 0, 0}}, {{(n-1)/2+1, n-1, n-1}});
-  box_t<3> freq({{0, 0, 0}}, {{(2*n-1)/2+1, 2*n-1, 2*n-1}});
+  box_t<3> sbox({{0, 0, 0}}, {{ns-1, ns-1, ns-1}});
+  box_t<3> dbox({{n-1-nd, n-1-nd, n-1-nd}}, {{n-1, n-1, n-1}});
+  box_t<3> rdomain({{0, 0, 0}}, {{n-1, n-1, n-1}});
+  box_t<3> freq({{0, 0, 0}}, {{(n-1)/2+1, n-1, n-1}});
 }
