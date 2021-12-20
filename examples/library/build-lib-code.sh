21f558359d5e0c61a53725c72307931f33d08099
broderickpt <Patrick.Broderick@gmail.com>
2021-12-20 16:16:14 -0500

    rework script

--- examples/library/build-lib-code.sh
+++ examples/library/build-lib-code.sh
@@ -3,56 +3,56 @@
 
 ##  Script to generate the source code for the libraries
 
-##  Return code from this script indicate if CPU, CUDA, or HIP code generated
-##    0 ==> Only CPU code generated
-##    1 ==> CUDA code generated
-##    2 ==> HIP code generated
+##  Returns the appropriate code gen flag for cmake:
+##    -D_codegen=CPU | -D_codegen=GPU | -D_codegen=HIP
 
-retcode=0
+FIL=precmake$$.txt
 
-echo "Always create the CPU code ... commands:"
+echo "Always create the CPU code ... commands:" > $FIL
 
-echo "python gen_files.py fftx_mddft cpu true nogen"
-echo "python gen_files.py fftx_mddft cpu false nogen"
-echo "python gen_files.py fftx_mdprdft cpu true nogen"
-echo "python gen_files.py fftx_mdprdft cpu false nogen"
-echo "python gen_files.py fftx_rconv cpu true nogen"
+echo "python gen_files.py fftx_mddft cpu true nogen" >> $FIL
+echo "python gen_files.py fftx_mddft cpu false nogen" >> $FIL
+echo "python gen_files.py fftx_mdprdft cpu true nogen" >> $FIL
+echo "python gen_files.py fftx_mdprdft cpu false nogen" >> $FIL
+echo "python gen_files.py fftx_rconv cpu true nogen" >> $FIL
 
 ##  Build DFT batch for CPU
 
-echo "python gen_dftbat.py fftx_dftbat cpu true nogen"
-echo "python gen_dftbat.py fftx_dftbat cpu false nogen"
-echo "python gen_dftbat.py fftx_prdftbat cpu true nogen"
-echo "python gen_dftbat.py fftx_prdftbat cpu false nogen"
+echo "python gen_dftbat.py fftx_dftbat cpu true nogen" >> $FIL
+echo "python gen_dftbat.py fftx_dftbat cpu false nogen" >> $FIL
+echo "python gen_dftbat.py fftx_prdftbat cpu true nogen" >> $FIL
+echo "python gen_dftbat.py fftx_prdftbat cpu false nogen" >> $FIL
+
+CODEGEN="-D_codegen=CPU"
 
 ##  Create code for a GPU if we find an appropriate compiler ...
 
-which nvcc
+which nvcc >> $FIL
 if [ $? -eq 0 ]; then
     ##  nvcc found -- generate Nvidia GPU (CUDA) code
-    echo "nvcc found ... generate CUDA code ... commands:"
-    echo "python gen_files.py fftx_mddft cuda true nogen"
-    echo "python gen_files.py fftx_mddft cuda false nogen"
-    echo "python gen_files.py fftx_mdprdft cuda true nogen"
-    echo "python gen_files.py fftx_mdprdft cuda false nogen"
-    echo "python gen_files.py fftx_rconv cuda true nogen"
-    retcode=1
+    echo "nvcc found ... generate CUDA code ... commands:" >> $FIL
+    echo "python gen_files.py fftx_mddft cuda true nogen" >> $FIL
+    echo "python gen_files.py fftx_mddft cuda false nogen" >> $FIL
+    echo "python gen_files.py fftx_mdprdft cuda true nogen" >> $FIL
+    echo "python gen_files.py fftx_mdprdft cuda false nogen" >> $FIL
+    echo "python gen_files.py fftx_rconv cuda true nogen" >> $FIL
+    CODEGEN="-D_codegen=GPU"
 else
-    echo "NVCC was not found -- DO NOT generate CUDA code"
+    echo "NVCC was not found -- DO NOT generate CUDA code" >> $FIL
 fi
 
-which hipcc
+which hipcc >> $FIL
 if [ $? -eq 0 ]; then
     ##  hipcc found -- generate AMD GPU (HIP) code
-    echo "hipcc found ... generate HIP code ... commands:"
-    echo "python gen_files.py fftx_mddft hip true nogen"
-    echo "python gen_files.py fftx_mddft hip false nogen"
-    echo "python gen_files.py fftx_mdprdft hip true nogen"
-    echo "python gen_files.py fftx_mdprdft hip false nogen"
-    echo "python gen_files.py fftx_rconv hip true nogen"
-    retcode=2
+    echo "hipcc found ... generate HIP code ... commands:" >> $FIL
+    echo "python gen_files.py fftx_mddft hip true nogen" >> $FIL
+    echo "python gen_files.py fftx_mddft hip false nogen" >> $FIL
+    echo "python gen_files.py fftx_mdprdft hip true nogen" >> $FIL
+    echo "python gen_files.py fftx_mdprdft hip false nogen" >> $FIL
+    echo "python gen_files.py fftx_rconv hip true nogen" >> $FIL
+    CODEGEN="-D_codegen=HIP"
 else
-    echo "HIPCC was not found -- DO NOT generate HIP code"
+    echo "HIPCC was not found -- DO NOT generate HIP code" >> $FIL
 fi
 
-exit $retcode
+echo $CODEGEN
