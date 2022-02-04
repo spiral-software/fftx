e94db697f996fc74c17b6edc530a824062831a2b
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2022-02-03 16:56:18 -0800

    On symmetric complex input, truncate the last dimension, not the first.

--- examples/library/transformer.fftx.precompile.hpp
+++ examples/library/transformer.fftx.precompile.hpp
@@ -146,7 +146,7 @@ namespace fftx {
     point_t<DIM> sizeHalf()
     {
       point_t<DIM> ret = m_size;
-      ret[0] = m_size[0]/2 + 1;
+      ret[DIM-1] = m_size[DIM-1]/2 + 1;
       return ret;
     }
 
