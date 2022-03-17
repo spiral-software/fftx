3e864e4856c8018e1aea140c8edfb5897f852073
Peter <PWMcCorquodale@lbl.gov>
2022-03-16 18:27:06 -0700

    Updated testhockney so that it's the same for HIP and CUDA and FFTX.

--- examples/hockney/testhockney.cpp
+++ examples/hockney/testhockney.cpp
@@ -3,7 +3,9 @@
 #include "fftx3utilities.h"
 #include "hockney.h"
 
-using namespace fftx;
+#ifdef FFTX_HIP
+#include "device_macros.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -18,12 +20,13 @@ int main(int argc, char* argv[])
     }
   printf("Running with verbosity %d\n", verbosity);
   printf("Set verbosity to 1 to check results, 2 to write all results.\n");
-  
-  // printf("%s: Entered test program.\n", argv[0]);
-	
-  array_t<3,double> G(hockney::rdomain);
 
-  // G on (0:2*n-1)^3.
+  // printf("%s: Entered test program.\n", argv[0]);
+
+  fftx::array_t<3, double> GHost(hockney::rdomain);
+  const double* GHostPtr = GHost.m_data.local();
+
+  // G on (0:n-1)^3.
   int midG = hockney::n / 2;
   forall([midG](double(&v), const fftx::point_t<3>& p)
          {
@@ -41,9 +44,37 @@ int main(int argc, char* argv[])
              {
                v = 0.;
              }
-         }, G);
+         }, GHost);
 
-  array_t<3, std::complex<double>> GhatComplex(hockney::freq);
+  fftx::array_t<3, std::complex<double>> GhatComplexHost(hockney::freq);
+  const std::complex<double>* GhatComplexHostPtr =
+    GhatComplexHost.m_data.local();
+
+#ifdef FFTX_HIP
+  double* GDevicePtr;
+  auto Gbytes = hockney::rdomain.size() * sizeof(double);
+  DEVICE_MALLOC(&GDevicePtr, GDevicebytes);
+  fftx::array_t<3, double> GDevice
+    (fftx::global_ptr<double>(GDevicePtr, 0, 1),
+     hockney::rdomain);
+  DEVICE_MEM_COPY(GDevicePtr, GHostPtr,
+                  Gbytes,
+                  MEM_COPY_HOST_TO_DEVICE);
+  
+  std::complex<double>* GhatComplexDevicePtr;
+  auto GhatComplexbytes = hockney::freq.size() * sizeof(std::complex<double>);
+  DEVICE_MALLOC(&GhatComplexDevicePtr, GhatComplexbytes);
+  fftx::array_t<3, std::complex<double>> GhatComplexDevice
+    (fftx::global_ptr<std::complex<double>>(GhatComplexDevicePtr, 0, 1),
+     hockney::freq);
+
+  fftx::array_t<3, double>& G = GDevice;
+  fftx::array_t<3, std::complex<double>>& GhatComplex = GhatComplexDevice;
+#else
+  fftx::array_t<3, double>& G = GHost;
+  fftx::array_t<3, std::complex<double>>& GhatComplex = GhatComplexHost;
+#endif
+
   printf("call gethat::init()\n");
   gethat::init();
   printf("call gethat::transform()\n");
@@ -52,13 +83,18 @@ int main(int argc, char* argv[])
          hockney::n, hockney::ns, hockney::nd, gethat::CPU_milliseconds);
   gethat::destroy();
 
-  { // Check that the imaginary part of GhatComplex is zero.
-      const std::complex<double>* GhatComplexPtr = GhatComplex.m_data.local();
+#ifdef FFTX_HIP
+  DEVICE_MEM_COPY(GhatComplexHostPtr, GhatComplexDevicePtr,
+                  GhatComplexbytes,
+                  MEM_COPY_DEVICE_TO_HOST);
+#endif
+
+  { // Check that the imaginary part of GhatComplexHost is zero.
       double totReal = 0.;
       double totImag = 0.;
-      for (int pt = 0; pt < GhatComplex.m_domain.size(); pt++)
+      for (int pt = 0; pt < GhatComplexHost.m_domain.size(); pt++)
         {
-          std::complex<double> GhatHere = GhatComplexPtr[pt];
+          std::complex<double> GhatHere = GhatComplexHostPtr[pt];
           totReal += std::abs(GhatHere.real());
           totImag += std::abs(GhatHere.imag());
         }
@@ -67,19 +103,34 @@ int main(int argc, char* argv[])
                 << std::endl;
   }
 
-  array_t<3, double> GhatReal(hockney::freq);
+  fftx::array_t<3, double> GhatRealHost(hockney::freq);
+  double* GhatRealHostPtr = GhatRealHost.m_data.local();
   {
-    const std::complex<double>* GhatComplexPtr = GhatComplex.m_data.local();
-    double* GhatRealPtr = GhatReal.m_data.local();
     for (int pt = 0; pt < hockney::freq.size(); pt++)
       {
-        GhatRealPtr[pt] = GhatComplexPtr[pt].real() /
+        GhatRealHostPtr[pt] = GhatComplexHostPtr[pt].real() /
           (hockney::rdomain.size()*1.);
       }
   }
-  
-  array_t<3,double> input(hockney::sbox);
-  array_t<3,double> output(hockney::dbox);
+
+#ifdef FFTX_HIP
+  fftx::array_t<3, double>* GhatRealDevicePtr;
+  auto GhatRealbytes = hockney::freq.size() * sizeof(double);
+  DEVICE_MALLOC(&GhatRealDevicePtr, GhatRealbytes);
+  fftx::array_t<3, double> GhatRealDevice
+    (fftx::global_ptr<double>(GhatRealDevicePtr, 0, 1),
+     hockney::freq);
+  DEVICE_MEM_COPY(GhatRealDevicePtr, GhatRealHostPtr,
+                  GhatRealbytes,
+                  MEM_COPY_HOST_TO_DEVICE);
+#else
+  fftx::array_t<3, double>& GhatReal = GhatRealHost;
+#endif
+
+  fftx::array_t<3,double> inputHost(hockney::sbox);
+  fftx::array_t<3,double> outputHost(hockney::dbox);
+  double* inputHostPtr = inputHost.m_data.local();
+  double* outputHostPtr = outputHost.m_data.local();
 
   // input has 1 inside sphere with this midpoint and radius
   int midinput = hockney::ns / 2;
@@ -87,7 +138,7 @@ int main(int argc, char* argv[])
   int radius2 = radius * radius;
   forall([midinput, radius2](double(&v), const fftx::point_t<3>& p)
          {
-           // if(p==point_t<3>({{2,2,2}}))  v=1.0;
+           // if(p==fftx::point_t<3>({{2,2,2}}))  v=1.0;
            // else  v=0.0;
            int sqsum = 0;
            for (int d = 0; d < 3; d++)
@@ -100,9 +151,33 @@ int main(int argc, char* argv[])
            else
              { v = 0.; }
            
-         }, input);
+         }, inputHost);
 
+#ifdef FFTX_HIP
+  double* inputDevicePtr;
+  auto inputbytes = hockney::sbox.size() * sizeof(double);
+  DEVICE_MALLOC(&inputDevicePtr, inputbytes);
+  fftx::array_t<3, double> inputDevice
+    (fftx::global_ptr<double>(inputDevicePtr, 0, 1),
+     hockney::sbox);
+  DEVICE_MEM_COPY(inputDevicePtr, inputHostPtr,
+                  inputbytes,
+                  MEM_COPY_HOST_TO_DEVICE);
   
+  double* outputDevicePtr;
+  auto outputbytes = hockney::dbox.size() * sizeof(double);
+  DEVICE_MALLOC(&outputDevicePtr, outputbytes);
+  fftx::array_t<3, double> outputDevice
+    (fftx::global_ptr<double>(outputDevicePtr, 0, 1),
+     hockney::dbox);
+  
+  fftx::array_t<3,double>& input = inputDevice;
+  fftx::array_t<3,double>& output = outputDevice;
+#else
+  fftx::array_t<3,double>& input = inputHost;
+  fftx::array_t<3,double>& output = outputHost;
+#endif
+
   printf("call hockney::init()\n");
   hockney::init();
   printf("call hockney::transform()\n");
@@ -111,26 +186,27 @@ int main(int argc, char* argv[])
          hockney::n, hockney::ns, hockney::nd, hockney::CPU_milliseconds);
   hockney::destroy();
 
+#ifdef FFTX_HIP
+  DEVICE_MEM_COPY(outputHostPtr, outputDevicePtr,
+                  outputbytes,
+                  MEM_COPY_DEVICE_TO_HOST);
+#endif
   if (verbosity >= 1)
     {
-      // box_t<3> sbox({{0, 0, 0}}, {{ns-1, ns-1, ns-1}});
+      // fftx::box_t<3> sbox({{0, 0, 0}}, {{ns-1, ns-1, ns-1}});
       int smin = 0;
       int smax = hockney::ns - 1;
-      // box_t<3> dbox({{n-1-nd, n-1-nd, n-1-nd}}, {{n-1, n-1, n-1}});
+      // fftx::box_t<3> dbox({{n-1-nd, n-1-nd, n-1-nd}}, {{n-1, n-1, n-1}});
       int dmin = hockney::n - 1 - hockney::nd;
       int dmax = hockney::n - 1;
-      // box_t<3> rdomain({{0, 0, 0}}, {{n-1, n-1, n-1}});
+      // fftx::box_t<3> rdomain({{0, 0, 0}}, {{n-1, n-1, n-1}});
       int Gmin = 0;
       int Gmax = hockney::n - 1;
       
-      double* outputData = output.m_data.local();
-      double* inputData = input.m_data.local();
-      double* GData = G.m_data.local();
-
       double outputMax = 0.;
       for (size_t pt = 0; pt < hockney::dbox.size(); pt++)
         {
-          updateMaxAbs(outputMax, outputData[pt]);
+          updateMaxAbs(outputMax, outputHostPtr[pt]);
         }
       std::cout << "Max absolute value of output is "
                 << outputMax << std::endl;
@@ -152,13 +228,15 @@ int main(int argc, char* argv[])
                 for (int js = smin; js <= smax; js++)
                   for (int ks = smin; ks <= smax; ks++)
                     {
-                      point_t<3> ps = point_t<3>({{is, js, ks}});
-                      point_t<3> pdiff = point_t<3>({{id-is, jd-js, kd-ks}});
+                      fftx::point_t<3> ps =
+                        fftx::point_t<3>({{is, js, ks}});
+                      fftx::point_t<3> pdiff =
+                        fftx::point_t<3>({{id-is, jd-js, kd-ks}});
                       directAns +=
-                        inputData[positionInBox(ps, hockney::sbox)] *
-                        GData[positionInBox(pdiff, hockney::rdomain)];
+                        inputHostPtr[positionInBox(ps, hockney::sbox)] *
+                        GHostPtr[positionInBox(pdiff, hockney::rdomain)];
                     }
-              double calcAns = outputData[ptOut];
+              double calcAns = outputHostPtr[ptOut];
               double diffAns = calcAns - directAns;
               if (verbosity >= 2)
                 {
@@ -170,7 +248,14 @@ int main(int argc, char* argv[])
             }
       std::cout << "Max absolute difference is " << diffAbsMax << std::endl;
     }
-  
+
+#ifdef FFTX_HIP
+  DEVICE_FREE(GDevicePtr);
+  DEVICE_FREE(GhatComplexDevicePtr);
+  DEVICE_FREE(GhatRealDevicePtr);
+  DEVICE_FREE(inputDevicePtr);
+  DEVICE_FREE(outputDevicePtr);
+#endif
   printf("%s: All done, exiting\n", argv[0]);
   return 0;
 }
