db2f1c9e92f77541bd71dfd25fec7f12b82e7033
broderickpt <Patrick.Broderick@gmail.com>
2022-04-07 18:20:58 -0400

    Changes to add MAC M1 architecture flag

--- examples/library/gen_files.py
+++ examples/library/gen_files.py
@@ -529,6 +529,7 @@ def cmake_library ( type ):
     _str = _str + 'add_library                ( ${_lib_name} SHARED ${_source_files} )\n'
     if type == 'CUDA':
         _str = _str + 'target_compile_options     ( ${_lib_name} PRIVATE ${CUDA_COMPILE_FLAGS} ${GPU_COMPILE_DEFNS} )\n'
+        _str = _str + 'target_compile_options     ( ${_lib_name} PRIVATE ${ADDL_COMPILE_FLAGS} )\n'
         _str = _str + 'set_property        ( TARGET ${_lib_name} PROPERTY CUDA_RESOLVE_DEVICE_SYMBOLS ON )\n\n'
     else:
         _str = _str + 'target_compile_options     ( ${_lib_name} PRIVATE ${HIP_COMPILE_FLAGS} ${ADDL_COMPILE_FLAGS} )\n\n'
