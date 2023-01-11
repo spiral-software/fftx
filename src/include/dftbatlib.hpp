53559e898dbb8f9422de9b005d67639d7806f717
Sanil Rao <sanilrao@login2.crusher.olcf.ornl.gov>
2023-01-11 14:06:19 -0500

    new changes to api and interface

--- src/include/dftbatlib.hpp
+++ src/include/dftbatlib.hpp
@@ -4,7 +4,7 @@ using namespace fftx;
 static constexpr auto dftbat_script{
 R"(
  ns := szns;
-    name := prefix::StringInt(nbatch)::"_"::StringInt(szns[1])::"_"::StringInt(Length(ns))::"d";
+    name := transform_spiral;
 
     t := let(batch := nbatch,
         apat := When(true, APar, AVec),
@@ -24,8 +24,11 @@ public:
     void semantics() {
         std::cout << "szns := [" << sizes.at(0) << "];" << std::endl;
         std::cout << "nbatch := " << sizes.at(1) << ";" << std::endl;
-         std::cout << "sign := -1;" << std::endl;
-        std::cout << "prefix := \"fftx_dftbat_\";" << std::endl;
+        std::cout << "sign := " << sizes.at(2) << ";" << std::endl;
+         if(sizes.at(3) == -1)
+            std::cout << "prefix := \"fftx_dftbat_\";" << std::endl;
+        else
+            std::cout << "prefix := \"fftx_idftbat_\";" << std::endl;
         std::cout << dftbat_script << std::endl;
     }
 };
