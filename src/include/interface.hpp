29febb14cba77852e4cae52356eecb3a825710b1
Sanil Rao <sanil@sanils-mbp.wifi.local.cmu.edu>
2023-02-06 12:27:36 -0500

    updated naming conventions for cached jit

--- src/include/interface.hpp
+++ src/include/interface.hpp
@@ -82,7 +82,7 @@ std::string getFFTX() {
         std::cout << "[ERROR] No such variable found, please download and set FFTX_HOME env variable" << std::endl;
         exit(-1);
     }
-    tmp += "/cached/"; 
+    tmp += "/cached_jit_files/"; 
     return tmp;
 }
 
@@ -116,13 +116,13 @@ void printJITBackend(std::string name, std::vector<int> sizes) {
     std::cout << "if 1 = 1 then opts:=conf.getOpts(transform);\ntt:= opts.tagIt(transform);\nif(IsBound(fftx_includes)) then opts.includes:=fftx_includes;fi;\nc:=opts.fftxGen(tt);\n fi;\n";
     std::cout << "GASMAN(\"collect\");\n";
     #if defined FFTX_HIP
-        std::cout << "PrintTo(\"" << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << ".txt\", PrintHIPJIT(c,opts));\n"; 
+        std::cout << "PrintTo(\"" << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_HIP" << ".txt\", PrintHIPJIT(c,opts));\n"; 
         std::cout << "PrintHIPJIT(c,opts);\n";
     #elif defined FFTX_CUDA 
-       std::cout << "PrintTo(\"" << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << ".txt\", PrintJIT2(c,opts));\n";   
+       std::cout << "PrintTo(\"" << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_CUDA" << ".txt\", PrintJIT2(c,opts));\n";   
        std::cout << "PrintJIT2(c,opts);\n";
     #else
-        std::cout << "PrintTo(\"" << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << ".txt\", opts.prettyPrint(c));\n"; 
+        std::cout << "PrintTo(\"" << tmp << "cached_" << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_CPU" << ".txt\", opts.prettyPrint(c));\n"; 
         std::cout << "opts.prettyPrint(c);\n";
     #endif
 }
@@ -216,7 +216,13 @@ void FFTXProblem::transform(){
         } else {
             std::ostringstream oss;
             std::string tmp = getFFTX();
-            oss << tmp << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << ".txt";
+            #if defined FFTX_HIP 
+            oss << tmp << "cached_" << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_HIP" << ".txt";
+            #elif defined FFTX_CUDA 
+            oss << tmp << "cached_" << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_CUDA" << ".txt";
+            #else
+            oss << tmp << "cached_" << name << "_" << sizes.at(0) << "x" << sizes.at(1) << "x" << sizes.at(2) << "_CPU" << ".txt";
+            #endif
             std::string file_name = oss.str();
             std::ifstream ifs ( file_name );
             if(ifs) {
