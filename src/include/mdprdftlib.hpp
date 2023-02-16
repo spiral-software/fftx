047a618ee1f14d4b08d8cd5b3d963107fdcd051b
Sanil Rao <sanilrao@login2.crusher.olcf.ornl.gov>
2023-02-16 15:44:59 -0500

    added transform header and files for extern fft apps

--- src/include/mdprdftlib.hpp
+++ src/include/mdprdftlib.hpp
@@ -1,25 +1,24 @@
 using namespace fftx;
 
+static constexpr auto mdprdft_script{
+    R"(szhalfcube := DropLast(szcube,1)::[Int(Last(szcube)/2)+1];
+    var_1:= var("var_1", BoxND([0,0,0], TReal));
+    var_2:= var("var_2", BoxND(szcube, TReal));
+    var_3:= var("var_3", BoxND(szhalfcube, TReal));
+    var_2:= X;
+    var_3:= Y;
+    symvar := var("sym", TPtr(TReal));
+    transform := TFCall(TDecl(TDAG([
+           TDAGNode(TTensorI(MDPRDFT(szcube,sign),1,APar, APar), var_3,var_2),
+                  ]),
+            [var_1]
+            ),
+        rec(fname:=name, params:= [symvar])
+    );
+    )"
+};
 
-static constexpr auto mddft_script{
-R"(
-var_1:= var("var_1", BoxND([0,0,0], TReal));
-var_2:= var("var_2", BoxND(szcube, TReal));
-var_3:= var("var_3", BoxND(szcube, TReal));
-var_2:= X;
-var_3:= Y;
-symvar := var("sym", TPtr(TReal));
-transform := TFCall(TDecl(TDAG([
-        TDAGNode(TTensorI(MDDFT(szcube,sign),1,APar, APar), var_3,var_2),
-                ]),
-        [var_1]
-        ),
-    rec(fname:=name, params:= [symvar])
-);
-)"};
-
-
-class MDDFTProblem: public FFTXProblem {
+class MDPRDFTProblem: public FFTXProblem {
 public:
     using FFTXProblem::FFTXProblem;
     void randomProblemInstance() {
@@ -28,11 +27,11 @@ public:
         std::cout << "szcube := [" << sizes.at(0) << ", " << sizes.at(1) << ", " << sizes.at(2) << "];" << std::endl;
         std::cout << "sign := -1;" << std::endl;
         std::cout << "name := \""<< name << "_spiral" << "\";" << std::endl;
-        std::cout << mddft_script << std::endl;
+        std::cout << mdprdft_script << std::endl;
     }
 };
 
-class IMDDFTProblem: public FFTXProblem {
+class IMDPRDFTProblem: public FFTXProblem {
 public:
     using FFTXProblem::FFTXProblem;
     void randomProblemInstance() {
@@ -41,6 +40,6 @@ public:
         std::cout << "szcube := [" << sizes.at(0) << ", " << sizes.at(1) << ", " << sizes.at(2) << "];" << std::endl;
         std::cout << "sign := 1;" << std::endl;
         std::cout << "name := \""<< name << "_spiral" << "\";" << std::endl;
-        std::cout << mddft_script << std::endl;
+        std::cout << mdprdft_script << std::endl;
     }
 };
