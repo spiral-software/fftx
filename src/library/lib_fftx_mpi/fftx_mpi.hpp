44db9deb4f812dfd43e89dc372539b13ae6d1d74
Tze Meng Low <lowt@thompopovici.andrew.cmu.edu>
2023-02-24 19:12:33 -0500

    Initial R2C implementation

--- src/library/lib_fftx_mpi/fftx_mpi.hpp
+++ src/library/lib_fftx_mpi/fftx_mpi.hpp
@@ -23,7 +23,7 @@
 
 using namespace std;
 
-#define CPU_PERMUTE 0     //Todo: Fix CPU PERMUTE to work with batch + embedded
+#define CPU_PERMUTE 1     //Todo: Fix CPU PERMUTE to work with batch + embedded
 #define CUDA_AWARE_MPI 0  //Todo: CUDA_AWARE_MPI not working
 
 // implement on GPU.
@@ -35,25 +35,26 @@ using namespace std;
 struct fftx_plan_t
 {
   complex<double> *recv_buffer, *send_buffer;
-  vector<int> shape;
   int r, c, b;
-  MPI_Comm row_comm, col_comm;
-  DEVICE_FFT_HANDLE stg3, stg2, stg1;
-  DEVICE_FFT_HANDLE stg2i, stg1i;  
   double  *Q3, *Q4;
   bool is_embed;
   bool is_forward;
+  bool is_complex;
+  MPI_Comm row_comm, col_comm;
+  size_t shape[6];  
+  DEVICE_FFT_HANDLE stg3, stg2, stg1;
+  DEVICE_FFT_HANDLE stg2i, stg1i;
 };
 
 typedef fftx_plan_t* fftx_plan;
 
 
-fftx_plan  fftx_plan_distributed(int r, int c, int M, int N, int K, int batch, bool is_embedded);
+fftx_plan  fftx_plan_distributed(int r, int c, int M, int N, int K, int batch, bool is_embedded, bool is_complex);
 void fftx_execute(fftx_plan plan, double* out_buffer, double*in_buffer,int direction);
 void fftx_plan_destroy(fftx_plan plan);
 
-void init_2d_comms(fftx_plan plan, int rr, int cc, int M, int N, int K, bool is_embedded);
-void destroy_2d_comms(fftx_plan plan);
+//void init_2d_comms(fftx_plan plan, int rr, int cc, int M, int N, int K, bool is_embedded);
+//void destroy_2d_comms(fftx_plan plan);
 
 // perm: [a, b, c] -> [a, c, b]
 void pack_embed(complex<double> *dst, complex<double> *src, int a, int b, int c, int batch, bool is_embedded);
