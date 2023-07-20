820a24f64756e227943f8983275ef85f5b855e44
Elliott Binder <elliottbinder@gmail.com>
2023-07-20 16:48:34 -0400

    add local 3d compute test for inv

--- examples/3DDFT_mpi/test3DDFT_mpi_1D.cpp
+++ examples/3DDFT_mpi/test3DDFT_mpi_1D.cpp
@@ -113,23 +113,27 @@ int main(int argc, char* argv[]) {
       for (int i = 0; i < M0; i++) {
         for (int l = 0; l < Ki; l++) {
           for (int b = 0; b < batch; b++) {
-            for (int c = 0; c < CI; c++) {
+            // for (int c = 0; c < CI; c++) {
+              int c = 0;
               // TODO: try with random data.
-              host_in[((j * M0*Ki + i * Ki + l)*batch + b) * CI + c] = {};
-            }
+              // host_in[((j * M0*Ki + i * Ki + l)*batch + b) * CI + c] = {};
+              host_in[((j * M0*Ki + i * Ki + l)*batch + b) * CI + c] = 1.0 * (b + 1) * (rank*1 + 1) * (i*1 + 1) * (j*1 + 1) * (l*1 + 1);
+
+              // host_in[((j * M0*Ki + i * Ki + l)*batch + b) * CI + c] = 1.0 * rand() / RAND_MAX;
+            // }
           }
         }
       }
     }
     // TODO: get rid of this when we have random data.
-    if (rank == 0) {
-      for (int b = 0; b < batch; b++) {
-        host_in[b * CI + 0] = (M * N * K * (b+1));
-        if (CI == 2) {
-          host_in[b * CI + 1] = 0;
-        }
-      }
-    }
+    // if (rank == 0) {
+    //   for (int b = 0; b < batch; b++) {
+    //     host_in[b * CI + 0] = (M * N * K * (b+1));
+    //     if (CI == 2) {
+    //       host_in[b * CI + 1] = 0;
+    //     }
+    //   }
+    // }
     DEVICE_MEM_COPY(dev_in, host_in, sizeof(double) * Ki * Ni * M0 * batch * CI, MEM_COPY_HOST_TO_DEVICE);
   } // end forward/inverse check.
 
@@ -153,56 +157,122 @@ int main(int argc, char* argv[]) {
   {
     // TODO: initially only support non-embedded C2C.
     // TODO: guard on volume of cube? maybe limit to 1 GB local mem or something reasonable.
-    if (!(is_embedded == false && is_forward == true && C2C == true)) {
+    if (!(is_embedded == false && C2C == true)) {
       if (rank == 0) {
         cout << "Initially only support local check for forward non-embedded C2C" << endl;
       }
     } else {
-      double *href_in, *href_out, *htest_out;
+      double *href_in, *href_in_perm, *href_out, *htest_out;
       double *dref_in, *dref_out;
       int root = 0;
       if (rank == root) {
         // TODO: fix o vs i here. embedded doesn't exist locally.?
-        href_in   = (double *) malloc(sizeof(double) * Ko * No * Mo * batch * CI);
-        href_out  = (double *) malloc(sizeof(double) * Ko * No * Mo * batch * CO);
-        htest_out = (double *) malloc(sizeof(double) * Ko * No * Mo * batch * CO);
-        DEVICE_MALLOC(&dref_in , sizeof(double) * Ko * No * Mo * batch * CI);
-        DEVICE_MALLOC(&dref_out, sizeof(double) * Ko * No * Mo * batch * CO);
+        size_t in_size, out_size;
+        if (is_forward) {
+          in_size  = Ko * No * Mo * batch * CI;
+          out_size = Ko * No * Mo * batch * CO;
+        } else {
+          in_size  = Ko * No * Mo * batch * CO;
+          out_size = Ko * No * Mo * batch * CI;
+        }
+          href_in   = (double *) malloc(sizeof(double) * in_size);
+          if (!is_forward) {
+            href_in_perm = (double *) malloc(sizeof(double) * in_size);
+          }
+          DEVICE_MALLOC(&dref_in , sizeof(double) * in_size);
+
+          href_out  = (double *) malloc(sizeof(double) * out_size);
+          htest_out = (double *) malloc(sizeof(double) * out_size);
+          DEVICE_MALLOC(&dref_out, sizeof(double) * out_size);
       }
       // gather all ins
-      int count = Ki/p * Ni * Mo * batch * CI;
+      int gather_in_count, gather_out_count;
+      if (is_forward) {
+        // TODO: change to K0?
+        // TODO: fix these?
+        // TODO: gather_in_count should match the size of input?
+        gather_in_count  = (Ki/p) * Ni * Mo * batch * CI;
+        gather_out_count = (Ki/p) * Ni * Mo * batch * CO;
+      } else {
+        // TODO: fix these?
+        gather_in_count  = (Ko  ) * No * M0 * batch * CO;
+        gather_out_count = (Ko  ) * No * M0 * batch * CI;
+      }
       // fwd [Z, Y, X, b] <= Gather pz on [(pz), Z/pz, Y, X, b]
       // inv [px, Y, X'/px, Z] <= Gather px on [(px), Y, X'/px, Z]
-      MPI_Gather(host_in, count, MPI_DOUBLE, href_in, count, MPI_DOUBLE, root, MPI_COMM_WORLD);
+      MPI_Gather(host_in, gather_in_count, MPI_DOUBLE, href_in, gather_in_count, MPI_DOUBLE, root, MPI_COMM_WORLD);
       // gather all outs
       // TODO update count for embedded.
       // fwd [px, Y, X'/px, Z] <= Gather px on [(px), Y, X'/px, Z]
-      DEVICE_MEM_COPY(host_out, dev_out, sizeof(double) * No * M0 * Ko * batch * CO, MEM_COPY_DEVICE_TO_HOST);
-      MPI_Gather(host_out, count, MPI_DOUBLE, htest_out, count, MPI_DOUBLE, root, MPI_COMM_WORLD);
+      // inv [Z, Y, X, b] Gather pz on [(pz), Z/pz, Y, X, b]
+      DEVICE_MEM_COPY(host_out, dev_out, sizeof(double) * gather_out_count, MEM_COPY_DEVICE_TO_HOST);
+      MPI_Gather(host_out, gather_out_count, MPI_DOUBLE, htest_out, gather_out_count, MPI_DOUBLE, root, MPI_COMM_WORLD);
       // TODO: permute to [Z, Y, X, b] for comparing against reference.
 
       // TODO: embed.?
       if (rank == root) {
+        // if inverse, permute data before calling cuFFT.
+        if (!is_forward) {
+          // inverse input  layout is [px, Y, X'/px, Z]
+          // inverse output layout is [Z, Y, X, b]
+          // permute data to [Z, Y, X, b] from [px, Y, X'/px, Z] before calling cuFFT.
+          for (int i1 = 0; i1 < M1; i1++) {
+            for (int j = 0; j < No; j++) {
+              for (int i0 = 0; i0 < M0; i0++) {
+                for (int k = 0; k < Ko; k++) {
+                  for (int b = 0; b < batch; b++) {
+                    for (int c = 0; c < CI; c++) {
+                    href_in_perm[((k * No*M1*M0 + j * M1*M0 + i1 * M0 + i0) * batch + b) * CI + c] = href_in[((i1 * No*M0*Ko + j * M0*Ko + i0 * Ko + k) * batch + b) * CI + c];
+                    }
+                  }
+                }
+              }
+            }
+          }
+          // swap pointers.
+          double *tmp = href_in_perm;
+          href_in_perm = href_in;
+          href_in = tmp;
+        }
+
         DEVICE_MEM_COPY(dref_in, href_in, sizeof(double) * Ko * No * Mo * batch * CI, MEM_COPY_HOST_TO_DEVICE);
         // TODO: change to vendor agnostic names.
         // create cuFFT plan 3d
         cufftHandle plan;
         // slowest to fastest.
         cufftPlan3d(&plan, Ko, No, Mo, CUFFT_Z2Z);
-        cufftExecZ2Z(plan, (cufftDoubleComplex *) dref_in, (cufftDoubleComplex *) dref_out, CUFFT_FORWARD);
+        cufftExecZ2Z(plan, (cufftDoubleComplex *) dref_in, (cufftDoubleComplex *) dref_out, is_forward ? CUFFT_FORWARD : CUFFT_INVERSE);
         DEVICE_MEM_COPY(href_out, dref_out, sizeof(double) * Ko * No * Mo * batch * CO, MEM_COPY_DEVICE_TO_HOST);
 
         // check href_out against htest_out.
-        for (int i = 0; i < M0; i++) {
-          for (int ii = 0; ii < M1; ii++) {
+        if (is_forward) {
+          for (int i = 0; i < M0; i++) {
+            for (int ii = 0; ii < M1; ii++) {
+              for (int j = 0; j < No; j++) {
+                for (int k = 0; k < Ko; k++) {
+                  for (int b = 0; b < batch; b++) {
+                    for (int c = 0; c < CO; c++) {
+                      int ref_idx = ((k * No*M0*M1 + j * M0*M1 + ii * M0 + i)*batch + b) * CO + c;
+                      int tst_idx = ((ii * No*M0*Ko + j * M0*Ko + i * Ko + k)*batch + b) * CO + c;
+                      if (abs(href_out[ref_idx] - htest_out[tst_idx]) > 1e-8) {
+                        cout << "Error: (" << k << "," << j << "," << i*M1 + ii << ")\t"<< "\t" << href_out[ref_idx] << " != " << htest_out[tst_idx] << endl;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        } else { // inverse
+          for (int k = 0; k < Ko; k++) {
             for (int j = 0; j < No; j++) {
-              for (int k = 0; k < Ko; k++) {
+              for (int i = 0; i < Mo; i++) {
                 for (int b = 0; b < batch; b++) {
                   for (int c = 0; c < CO; c++) {
-                    int ref_idx = ((k * No*M0*M1 + j * M0*M1 + ii * M0 + i)*batch + b) * CO + c;
-                    int tst_idx = ((ii * No*M0*Ko + j * M0*Ko + i * Ko + k)*batch + b) * CO + c;
+                    int ref_idx = ((k * No*Mo + j * Mo + i)*batch + b) * CO + c;
+                    int tst_idx = ((k * No*Mo + j * Mo + i)*batch + b) * CO + c;
                     if (abs(href_out[ref_idx] - htest_out[tst_idx]) > 1e-8) {
-                      cout << "Error: (" << k << "," << j << "," << i*M1 + ii << ")\t"<< "\t" << href_out[ref_idx] << " != " << htest_out[tst_idx] << endl;
+                      cout << "Error: (" << k+1 << "," << j+1 << "," << i+1 << ")\t"<< "\t" << href_in[ref_idx] << "\t" << href_out[ref_idx] << " != " << htest_out[tst_idx] << endl;
                     }
                   }
                 }
@@ -227,68 +297,68 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  // Check first element.
-  if (is_forward) {
-    // TODO: copy more for C2R?
-    int Mdim = R2C ? (Mo/2+1)/p : Mo/p;
-    Mdim += Mdim * p < Mo;
+  // // Check first element.
+  // if (is_forward) {
+  //   // TODO: copy more for C2R?
+  //   int Mdim = R2C ? (Mo/2+1)/p : Mo/p;
+  //   Mdim += Mdim * p < Mo;
 
-    DEVICE_MEM_COPY(host_out, dev_out, sizeof(complex<double>) * No * Mdim * Ko * batch, MEM_COPY_DEVICE_TO_HOST);
+  //   DEVICE_MEM_COPY(host_out, dev_out, sizeof(complex<double>) * No * Mdim * Ko * batch, MEM_COPY_DEVICE_TO_HOST);
 
-    double *first_elems = (double *) malloc(sizeof(double) * batch);
-    for (int b = 0; b < batch; b++) {
-      first_elems[b] = {};
-    }
+  //   double *first_elems = (double *) malloc(sizeof(double) * batch);
+  //   for (int b = 0; b < batch; b++) {
+  //     first_elems[b] = {};
+  //   }
 
-    // initial distribution is [Z/p, Y, X, b]
-    for (int l = 0; l < Ki/p; l++) {
-      for (int j = 0; j < Ni; j++) {
-        for (int i = 0; i < Mo; i++) {
-          for (int b = 0; b < batch; b++) {
-            first_elems[b] += host_in[((l * Ni*Mo + j * Mo + i)*batch + b)*CI + 0];
-            // skip imaginary elements.
-          }
-        }
-      }
-    }
+  //   // initial distribution is [Z/p, Y, X, b]
+  //   for (int l = 0; l < Ki/p; l++) {
+  //     for (int j = 0; j < Ni; j++) {
+  //       for (int i = 0; i < Mo; i++) {
+  //         for (int b = 0; b < batch; b++) {
+  //           first_elems[b] += host_in[((l * Ni*Mo + j * Mo + i)*batch + b)*CI + 0];
+  //           // skip imaginary elements.
+  //         }
+  //       }
+  //     }
+  //   }
 
-    for (int b = 0; b < batch; b++) {
-      MPI_Allreduce(MPI_IN_PLACE, first_elems + b, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-    }
+  //   for (int b = 0; b < batch; b++) {
+  //     MPI_Allreduce(MPI_IN_PLACE, first_elems + b, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  //   }
 
-    // distribution is [Y, X'/p, Z, b]
-    if (rank == 0) {
-      printf("diff:\n");
-      for (int b = 0; b < batch; b++) {
-        printf(
-          "%16f %16f %16f\n",
-          first_elems[b],
-          host_out[b*CI + 0],
-          first_elems[b] - host_out[b*CI + 0]
-        );
-      }
-      printf("\n");
-    }
-    free(first_elems);
-  } else { // C2R
-    // TODO: fix this
-    int Mdim = C2R ? (Mo/2+1)/p : Mo/p;
-    Mdim += Mdim * p < Mo;
+  //   // distribution is [Y, X'/p, Z, b]
+  //   if (rank == 0) {
+  //     printf("diff:\n");
+  //     for (int b = 0; b < batch; b++) {
+  //       printf(
+  //         "%16f %16f %16f\n",
+  //         first_elems[b],
+  //         host_out[b*CI + 0],
+  //         first_elems[b] - host_out[b*CI + 0]
+  //       );
+  //     }
+  //     printf("\n");
+  //   }
+  //   free(first_elems);
+  // } else { // C2R
+  //   // TODO: fix this
+  //   int Mdim = C2R ? (Mo/2+1)/p : Mo/p;
+  //   Mdim += Mdim * p < Mo;
 
-    DEVICE_MEM_COPY(host_out, dev_out, sizeof(complex<double>) * No * Mdim * Ko * batch, MEM_COPY_DEVICE_TO_HOST);
-    if (rank == 0) {
-      printf("diff:\n");
-      for (int b = 0; b < batch; b++) {
-        printf(
-          "%16f %16f %16f\t",
-          (double) (M * N * K * (b+1)),
-          ((complex<double> *)host_out)[b].real(),
-          (double) (M * N * K * (b+1)) - ((complex<double> *)host_out)[b].real()
-        );
-        printf("\n");
-      }
-    }
-  }
+  //   DEVICE_MEM_COPY(host_out, dev_out, sizeof(complex<double>) * No * Mdim * Ko * batch, MEM_COPY_DEVICE_TO_HOST);
+  //   if (rank == 0) {
+  //     printf("diff:\n");
+  //     for (int b = 0; b < batch; b++) {
+  //       printf(
+  //         "%16f %16f %16f\t",
+  //         (double) (M * N * K * (b+1)),
+  //         ((complex<double> *)host_out)[b].real(),
+  //         (double) (M * N * K * (b+1)) - ((complex<double> *)host_out)[b].real()
+  //       );
+  //       printf("\n");
+  //     }
+  //   }
+  // }
 
   fftx_plan_destroy(plan);
 
