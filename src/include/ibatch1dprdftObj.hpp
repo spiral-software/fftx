4fe1d89b57526152099084079c136ed7cd16f862
Sanil Rao <sanilrao@login09.frontier.olcf.ornl.gov>
2023-09-06 17:03:37 -0400

    integrated spiral backend R2C support for distributed 1d

---
+++ src/include/ibatch1dprdftObj.hpp
@@ -0,0 +1,33 @@
+using namespace fftx;
+
+static std::string ibatch1dprdft_script = "transform := let(\n\
+         TFCall(TRC(TTensorI(PRDFT(N, sign), B, write, read)),\n\
+            rec(fname := name, params := [])));";
+
+class IBATCH1DPRDFTProblem: public FFTXProblem {
+public:
+    using FFTXProblem::FFTXProblem;
+    void randomProblemInstance() {
+    }
+    void semantics() {
+        std:cout << "Import(realdft);" << std::endl;
+        std::cout << "N := " << sizes.at(0) << ";" << std::endl;
+        std::cout << "B := " << sizes.at(1) << ";" << std::endl;
+        if(sizes.at(2) == 0) {
+            std::cout << "read := APar;" << std::endl;
+        }
+        else{
+            std::cout << "read := AVec;" << std::endl;
+        }
+        if(sizes.at(3) == 0) {
+            std::cout << "write := APar;" << std::endl;
+        }
+        else{
+            std::cout << "write := AVec;" << std::endl;
+        }
+        std::cout << "sign := 1;" << std::endl;
+        std::cout << "name := \""<< name << "_spiral" << "\";" << std::endl;
+        std::cout << ibatch1dprdft_script << std::endl;
+    }
+};
+
