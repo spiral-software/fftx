f80e5f4f478c459d0592e7b84f7e9c9ebb436e05
Elliott Binder <elliottbinder@gmail.com>
2023-10-24 11:38:59 -0400

    changes for larger sized distributed FFTs

--- examples/3DDFT_mpi/test3DDFT_mpi_2D.cpp
+++ examples/3DDFT_mpi/test3DDFT_mpi_2D.cpp
@@ -36,38 +36,34 @@ int main(int argc, char* argv[]) {
   int r = grid;
   int c = grid;
 
-
   // 3d fft sizes
-  int Mi, Ni, Ki;
-  int Mo, No, Ko;
-  
+  uint64_t Mi, Ni, Ki;
+  uint64_t Mo, No, Ko;
+
   Mi = M;
   Ni = N;
   Ki = K;
   Mo = M * (is_embedded ? 2 : 1);
   No = N * (is_embedded ? 2 : 1);
   Ko = K * (is_embedded ? 2 : 1);
-  
+
   //define device buffers
   double *in_buffer = NULL;
   complex<double> *out_buffer = NULL;
 
-  //define host buffers  
+  //define host buffers
   double *fftx_in;
   complex<double> *fftx_out;
 
 
   //embedded requires dim Z to be padded to full size (Ko instead of Ki)
-  if (is_complex)
-    {
-      fftx_in = new double[ Ko * Mi * Ni/p * batch * 2];
-      fftx_out = new complex<double>[ Mo * No * Ko/p * batch];
-    }
-  else
-    {
-      fftx_in = new double[ Ko * Mi * Ni/p * batch];
-      fftx_out = new complex<double>[ Mo * No * Ko/p * batch];  //does this need to be padded further?
-    }
+  if (is_complex) {
+    fftx_in = new double[Ko * Mi * Ni/p * batch * 2];
+    fftx_out = new complex<double>[Mo * No * Ko/p * batch];
+  } else {
+    fftx_in = new double[Ko * Mi * Ni/p * batch];
+    fftx_out = new complex<double>[Mo * No * Ko/p * batch];  //does this need to be padded further?
+  }
 
   //allocate buffers
   DEVICE_ERROR_T err = DEVICE_MALLOC(&in_buffer, Ko*Mi*Ni/p * (is_complex ? sizeof(complex<double>): sizeof(double))  * batch);
@@ -75,63 +71,58 @@ int main(int argc, char* argv[]) {
     cout << "DEVICE_MALLOC failed\n" << endl;
     exit(-1);
   }
-  
+
   err = DEVICE_MALLOC(&out_buffer, Mo*No*Ko/p * sizeof(complex<double>) * batch);
   if (err != DEVICE_SUCCESS) {
     cout << "DEVICE_MALLOC failed\n" << endl;
     exit(-1);
   }
 
-  // initialize data to random values in the range (-1, 1). 
+  // initialize data to random values in the range (-1, 1).
   // assume data is padded in one dim as input.
-  for (int n = 0; n < Ni/r; n++) {
-    for (int m = 0; m < Mi/c; m++) {
-      for (int k = 0; k < Ko; k++) {
-        for (int b = 0; b < batch; b++) {
-          fftx_in[
-            ((n * (Mi/c)*Ko +
-             m *        Ko +
-             k)* batch     +
-             b)               * (is_complex ? 2 : 1) + 0
-            ] = ( // embedded
+  uint64_t cmplx = is_complex ? 2 : 1;
+  for (uint64_t n = 0; n < Ni/r; n++) {
+    for (uint64_t m = 0; m < Mi/c; m++) {
+      for (uint64_t k = 0; k < Ko; k++) {
+        for (uint64_t b = 0; b < batch; b++) {
+          double *in = fftx_in +
+            n * (Mi/c) * Ko * batch * cmplx +
+            m          * Ko * batch * cmplx +
+            k               * batch * cmplx +
+            b                       * cmplx +
+            0
+          ;
+          in[0] = (
+            (Ki/2 <= k && k < 3*Ki/2 ) ||
+            !is_embedded
+          ) ?
+          //1 - ((double) rand()) / (double) (RAND_MAX/2),
+          (is_forward ? (b+1) : 0)
+          :
+          0;
+          if (is_complex) { // set real component.
+            in[1] = (
               (Ki/2 <= k && k < 3*Ki/2 ) ||
               !is_embedded
-		) ? 	    
-	       //1 - ((double) rand()) / (double) (RAND_MAX/2),
-	    (is_forward ? (b+1) : 0)
-	    :
-	    0;
-
-	  if (is_complex)
-	    {
-          fftx_in[
-	   ((n * (Mi/c)*Ko +
-             m *        Ko +
-             k)* batch     +
-	     b)               * 2 + 1
-            ] = ( // embedded
-              (Ki/2 <= k && k < 3*Ki/2 ) ||
-              !is_embedded
-		) ? 	    
-	       //1 - ((double) rand()) / (double) (RAND_MAX/2),
-	    (is_forward ? (b+1) : 0)
-	    :
-	    0;	      
-	    }
+            ) ?
+              //1 - ((double) rand()) / (double) (RAND_MAX/2),
+              (is_forward ? (b+1) : 0)
+              :
+              0;
+          }
         }
       }
-      }
+    }
   }
-
 
   if (!is_forward) {
     if (commRank == 0) {
-      for (int b = 0; b < batch; b++)
-	{
-	  fftx_in[b*2 + 0] = (M*N*K*(b+1));
-	  if (is_complex)
-	    fftx_in[b*2 + 1] = (M*N*K*(b+1));	    
-	}
+      for (int b = 0; b < batch; b++) {
+        fftx_in[b*2 + 0] = (M*N*K*(b+1));
+        if (is_complex) {
+          fftx_in[b*2 + 1] = (M*N*K*(b+1));
+        }
+      }
     }
   }
   //end init
@@ -145,40 +136,40 @@ int main(int argc, char* argv[]) {
   fftx_plan  plan = fftx_plan_distributed(r, c, M, N, K, batch, is_embedded, is_complex);
 
   DEVICE_SYNCHRONIZE();
-  MPI_Barrier(MPI_COMM_WORLD); 
-  
+  MPI_Barrier(MPI_COMM_WORLD);
+
   if (commRank == 0) {
-    cout<<"Problem size: "<<M<<" x "<<N<<" x "<<K<<endl;
-    cout<<"Batch size  : "<<batch<<endl;
-    cout<<"Complex     : "<<(is_complex ? "Yes" : "No")<<endl;
-    cout<<"Embedded    : "<<(is_embedded ? "Yes": "No")<<endl;
-    cout<<"Direction   : "<<(is_forward ? "Forward": "Inverse")<<endl;
-    cout<<"Grid size   : "<<r<<" x "<<c<<endl;
+    cout << "Problem size    : " << M << " x " << N << " x " << K << endl;
+    cout << "Batch size      : " << batch << endl;
+    cout << "Complex         : " << (is_complex ? "Yes" : "No") << endl;
+    cout << "Embedded        : " << (is_embedded ? "Yes": "No") << endl;
+    cout << "Direction       : " << (is_forward ? "Forward": "Inverse") << endl;
+    cout << "Grid size       : " << r << " x " << c << endl;
   }
 
-  for (int t = 0; t < 1; t++) {
+  for (int t = 0; t < 3; t++) {
 
     double start_time = MPI_Wtime();
-    
+
     fftx_execute(plan, (double*)out_buffer, (double*)in_buffer, (is_forward ? DEVICE_FFT_FORWARD: DEVICE_FFT_INVERSE));
-    
+
     double end_time = MPI_Wtime();
-    
+
     // double min_time    = min_diff(start_time, end_time, MPI_COMM_WORLD);
     double max_time    = max_diff(start_time, end_time, MPI_COMM_WORLD);
 
     DEVICE_MEM_COPY(fftx_out, out_buffer, (Mo*No*Ko/p) * sizeof(complex<double>)*batch, MEM_COPY_DEVICE_TO_HOST);
     DEVICE_SYNCHRONIZE();
-        
+
     if (commRank == 0) {
-      cout<<endl<<"end_to_end," << max_time<<endl;      
+      // cout<<endl<<"end_to_end," << max_time<<endl;
+      cout<<endl;
     }
-
-    
   }
-  MPI_Barrier(MPI_COMM_WORLD); 
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  for (int rank = 0; rank < p; ++rank){
+  // for (int rank = 0; rank < p; ++rank){
+  for (int rank = 0; rank < 1; ++rank){
     if (rank == commRank){
       cout<<commRank<<": ";
 
@@ -191,11 +182,11 @@ int main(int argc, char* argv[]) {
 	cout<<fftx_out[b].real()<<" ";
       }
       cout<<endl;
-      
+
     }
     MPI_Barrier(MPI_COMM_WORLD);
   }
-  
+
  fftx_plan_destroy(plan);
 
  MPI_Finalize();
