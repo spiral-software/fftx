d94bbaaa53695fa3de7d8148202f31948b9d2b4b
petermcLBL <PWMcCorquodale@lbl.gov>
2024-05-07 17:35:29 -0700

    exec takes T_IN* and T_OUT*.

--- examples/verify/VerifyTransform.hpp
+++ examples/verify/VerifyTransform.hpp
@@ -367,8 +367,8 @@ public:
 	// 2. Perform the transform.
         if (m_tp == DEVICE_LIB)
           {
-            DEVICE_FFT_CHECK(m_deviceTfm3dPtr->exec((void*)inputDevicePtr,
-                                                    (void*)outputDevicePtr),
+            DEVICE_FFT_CHECK(m_deviceTfm3dPtr->exec((T_IN*)inputDevicePtr,
+                                                    (T_OUT*)outputDevicePtr),
                              "device FFT exec launch");
           }
         else
