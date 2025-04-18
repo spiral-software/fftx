302bb74f35758c7e2e9c3c61f303ee8bc71b04fd
petermcLBL <PWMcCorquodale@lbl.gov>
2025-04-18 14:17:22 -0700

    Changed number of iterations in test suite runs to 5.

--- test_suite.sh
+++ test_suite.sh
@@ -10,36 +10,36 @@
 # in order to run the script.
 #
 
-./bin/testbatch1ddft -s 256x64
-./bin/testbatch1ddft -s 512x32
-./bin/testbatch1ddft -s 1024x16
+./bin/testbatch1ddft -i 5 -s 256x64
+./bin/testbatch1ddft -i 5 -s 512x32
+./bin/testbatch1ddft -i 5 -s 1024x16
 
-./bin/testbatch1dprdft -s 256x64
-./bin/testbatch1dprdft -s 512x32
-./bin/testbatch1dprdft -s 1024x16
+./bin/testbatch1dprdft -i 5 -s 256x64
+./bin/testbatch1dprdft -i 5 -s 512x32
+./bin/testbatch1dprdft -i 5 -s 1024x16
 
-./bin/testmddft -s 40x40x40
-./bin/testmddft -s 72x72x72
-./bin/testmddft -s 128x128x128
+./bin/testmddft -i 5 -s 40x40x40
+./bin/testmddft -i 5 -s 72x72x72
+./bin/testmddft -i 5 -s 128x128x128
 
-./bin/testmdprdft -s 40x40x40
-./bin/testmdprdft -s 72x72x72
-./bin/testmdprdft -s 128x128x128
+./bin/testmdprdft -i 5 -s 40x40x40
+./bin/testmdprdft -i 5 -s 72x72x72
+./bin/testmdprdft -i 5 -s 128x128x128
 
-./bin/testhockneyconv -s 32x32x128
-./bin/testhockneyconv -s 32x128x32
-./bin/testhockneyconv -s 128x32x32
-./bin/testhockneyconv -s 64x64x64
+./bin/testhockneyconv -i 5 -s 32x32x128
+./bin/testhockneyconv -i 5 -s 32x128x32
+./bin/testhockneyconv -i 5 -s 128x32x32
+./bin/testhockneyconv -i 5 -s 64x64x64
 
 ./bin/testrconv_lib
-./bin/testrconv -s 40x40x40
-./bin/testrconv -s 72x72x72
-./bin/testrconv -s 128x128x128
+./bin/testrconv -i 5 -s 40x40x40
+./bin/testrconv -i 5 -s 72x72x72
+./bin/testrconv -i 5 -s 128x128x128
 
 ./bin/testverify_lib
-./bin/testverify -s 40x40x40
-./bin/testverify -s 72x72x72
-./bin/testverify -s 128x128x128
+./bin/testverify -i 5 -s 40x40x40
+./bin/testverify -i 5 -s 72x72x72
+./bin/testverify -i 5 -s 128x128x128
 
 ./bin/fortran_main 32 32 32
 ./bin/fortran_main 64 64 64
