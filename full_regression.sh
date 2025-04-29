827460d648ca4551486065a8e4c416ab948015a7
petermcLBL <PWMcCorquodale@lbl.gov>
2025-04-29 00:03:41 -0700

    Corrected directories for regression test.

--- full_regression.sh
+++ full_regression.sh
@@ -3,10 +3,7 @@ timestamp=`date +%G%m%d_%H%M`
 statusfile=$HOME/regression_status_$timestamp.txt
 outputfile=$HOME/regression_output_$timestamp.txt
 # Get and install SPIRAL, and set SPIRAL_HOME to its location.
-# Should have spiral-software and fftx at same directory level.
-pushd ..
-  source get_install_spiral.sh
-popd
+source get_install_spiral.sh
 if [ -f "$SPIRAL_HOME/bin/spiral" ]; then
     pushd fftx
       # Install FFTX, and set FFTX_HOME to its location.
--- get_install_spiral.sh
+++ get_install_spiral.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-git clone -b develop https://github.com/spiral-software/spiral-software.git
-export SPIRAL_HOME=$PWD/spiral-software
-pushd $SPIRAL_HOME/namespaces/packages
-  git clone -b develop https://www.github.com/spiral-software/spiral-package-fftx fftx
-  git clone -b develop https://www.github.com/spiral-software/spiral-package-simt simt
-  git clone -b develop https://www.github.com/spiral-software/spiral-package-mpi mpi
-  git clone -b develop https://www.github.com/spiral-software/spiral-package-jit jit
-popd
-pushd $SPIRAL_HOME
+git clone -b develop https://github.com/spiral-software/spiral-software.git ../spiral-software
+pushd ../spiral-software
+  export SPIRAL_HOME=$PWD
+  pushd namespaces/packages
+    git clone -b develop https://www.github.com/spiral-software/spiral-package-fftx fftx
+    git clone -b develop https://www.github.com/spiral-software/spiral-package-simt simt
+    git clone -b develop https://www.github.com/spiral-software/spiral-package-mpi mpi
+    git clone -b develop https://www.github.com/spiral-software/spiral-package-jit jit
+  popd
   mkdir -p build
   pushd build
     cmake ..
