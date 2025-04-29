291321f923d79db375374daaa4f30a3a9a9847a4
petermcLBL <PWMcCorquodale@lbl.gov>
2025-04-28 23:49:40 -0700

    corrections to cron script

--- full_regression.sh
+++ full_regression.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 timestamp=`date +%G%m%d_%H%M`
-mkdir -p /tmp/regression_$timestamp
 statusfile=$HOME/regression_status_$timestamp.txt
 outputfile=$HOME/regression_output_$timestamp.txt
 # Get and install SPIRAL, and set SPIRAL_HOME to its location.
-source get_install_spiral.sh
+# Should have spiral-software and fftx at same directory level.
+pushd ..
+  source get_install_spiral.sh
+popd
 if [ -f "$SPIRAL_HOME/bin/spiral" ]; then
-    # Install FFTX, and set FFTX_HOME to its location.
-    source install_fftx.sh
-    pushd $FFTX_HOME
-       if [ -d "bin" ]; then
+    pushd fftx
+      # Install FFTX, and set FFTX_HOME to its location.
+      source install_fftx.sh
+      if [ -d "bin" ]; then
           source bash_test_suite.sh $statusfile > $outputfile
-       else
+      else
           echo "FAILED: FFTX not installed" > $statusfile
-       fi
+      fi
     popd
 else
     echo "FAILED: Spiral not installed" > $statusfile
--- install_fftx.sh
+++ install_fftx.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-# Assumes FFTX is already cloned into directory fftx.
-export FFTX_HOME=$PWD/fftx
-pushd $FFTX_HOME
-  ./config-fftx-libs.sh CPU
-  mkdir -p build
-  pushd build
-    cmake -DCMAKE_INSTALL_PREFIX=$FFTX_HOME -D_codegen=CPU ..
-  popd
+# Assumes FFTX is already cloned into directory fftx, and we are in it.
+export FFTX_HOME=$PWD
+./config-fftx-libs.sh CPU
+mkdir -p build
+pushd build
+  cmake -DCMAKE_INSTALL_PREFIX=$FFTX_HOME -D_codegen=CPU ..
 popd
