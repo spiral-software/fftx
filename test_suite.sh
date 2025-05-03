6258afe87c9950b5d2242ebcd565cfe6a971822a
petermcLBL <PWMcCorquodale@lbl.gov>
2025-05-02 19:24:40 -0700

    Changed arguments to Fortran program, and added timings.

--- test_suite.sh
+++ test_suite.sh
@@ -8,10 +8,24 @@
 ##  in order to run the script.
 ##
 
+##  Function to time a run and report pass or fail
+time_test() {
+    start_time=`date +%s.%N`
+    eval $1
+    rc=$?
+    end_time=`date +%s.%N`
+    runtime=$( echo "$end_time - $start_time" | bc -l )
+    if [[ $rc = 0 ]]; then
+        echo "PASSED $1 in $runtime sec"
+    else
+        echo "FAILED $1 in $runtime sec"
+    fi
+}
+
 ##  Function to run tests for an executable
 run_tests() {
     local base=$1
-    local is_fortran=$2
+    local itns=$2
     shift 2                             ##  Remove first two arguments
     local sizes=("$@")                  ##  Capture all remaining arguments as sizes
 
@@ -22,23 +36,24 @@ run_tests() {
         exen="./bin/${base}"
     fi
 
+    ## Set itnspec to "-i $itns" if itns is nonzero, otherwise blank
+    if [[ "$itns" -eq 0 ]]; then
+        itnspec=""
+    else
+        itnspec="-i $itns"
+    fi
+
     ##  Check if executable exists and run tests
     if [ -f "$exen" ]; then
         echo "Running tests for $base..."
-        if [ "$is_fortran" = true ]; then
-            ##  For Fortran-specific arguments
-            for xx in "${sizes[@]}"; do
-                echo "Run $base size = $xx"
-                $exen $xx             ##  No additional quotes needed
-            done
-        elif [ "${#sizes[@]}" -eq 0 ]; then
+        if [ "${#sizes[@]}" -eq 0 ]; then
             ##  No sizes argument, run the program with just the iteration switch
-            $exen -i 5
+           time_test "$exen $itnspec"
         else
             ##  Iterate runs of the program with each specified size
             for xx in "${sizes[@]}"; do
                 echo "Run $base size = $xx"
-                $exen -i 5 -s "$xx"
+                time_test "$exen $itnspec -s $xx"
             done
         fi
     else
@@ -47,16 +62,16 @@ run_tests() {
 }
 
 ##  Test definitions
-run_tests "testbatch1ddft"      false   256x64 512x32 1024x16
-run_tests "testbatch1dprdft"    false   256x64 512x32 1024x16
-run_tests "testmddft"           false   40x40x40 64x64x64 72x72x72 128x128x128
-run_tests "testmdprdft"         false   40x40x40 64x64x64 72x72x72 128x128x128
-run_tests "testhockneyconv"     false   32x32x128 32x128x32 128x32x32 64x64x64
+run_tests "fortran_main"        0   47x41x43
+run_tests "testbatch1ddft"      5   256x64 512x32 1024x16
+run_tests "testbatch1dprdft"    5   256x64 512x32 1024x16
+run_tests "testmddft"           5   40x40x40 64x64x64 72x72x72 128x128x128
+run_tests "testmdprdft"         5   40x40x40 64x64x64 72x72x72 128x128x128
+run_tests "testhockneyconv"     5   32x32x128 32x128x32 128x32x32 64x64x64
 
-run_tests "testrconv_lib"       false           ##  No sizes
-run_tests "testrconv"           false   40x40x40 64x64x64 72x72x72 128x128x128
-run_tests "testverify_lib"      false           ##  No sizes
-run_tests "testverify"          false   40x40x40 64x64x64 72x72x72 128x128x128
+run_tests "testrconv_lib"       5           ##  No sizes
+run_tests "testrconv"           5   40x40x40 64x64x64 72x72x72 128x128x128
+run_tests "testverify_lib"      5           ##  No sizes
+run_tests "testverify"          5   40x40x40 64x64x64 72x72x72 128x128x128
 
-##  We need to specify the fortran sizes differently because we need embedded spaces...
-run_tests "fortran_main"        true    "32 32 32 " "64 64 64"
+run_tests "fortran_main"        0   32x32x32 64x64x64
