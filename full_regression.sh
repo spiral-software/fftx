bf8040decb372658feaab304e4379d00d0093b88
petermcLBL <PWMcCorquodale@lbl.gov>
2025-05-05 15:05:59 -0700

    Add ability to run regression script on non-CPU backends.

--- full_regression.sh
+++ full_regression.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "Starting `date`"
+# For install_fftx.sh,
+# need _codegen set to one of: CPU CUDA HIP SYCL;
+# optional _compilerspec set to -DCMAKE_CXX_COMPILER=(compiler).
+echo "Starting full_regression.sh at `date`"
 source get_install_spiral.sh
 if [ -f "$SPIRAL_HOME/bin/spiral" ]; then
     echo "SUCCESS: Spiral installed at $SPIRAL_HOME/bin/spiral"
@@ -14,3 +17,4 @@ if [ -f "$SPIRAL_HOME/bin/spiral" ]; then
 else
     echo "FAILED: Spiral not installed at $SPIRAL_HOME"
 fi
+echo "Finished full_regression at `date`"
--- install_fftx.sh
+++ install_fftx.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# Called from full_regression.sh.
 # Assumes FFTX is already cloned into directory fftx, and we are in it.
+# Need _codegen set to one of: CPU CUDA HIP SYCL.
+# Optional _compilerspec set to -DCMAKE_CXX_COMPILER=(compiler)
 export FFTX_HOME=$PWD
-./config-fftx-libs.sh CPU > config-fftx-libs.out
+./config-fftx-libs.sh $_codegen > config-fftx-libs.out
 mkdir -p build
 pushd build
-  cmake -DCMAKE_INSTALL_PREFIX=$FFTX_HOME -D_codegen=CPU .. > ../cmake.out
+  cmake -DCMAKE_INSTALL_PREFIX=$FFTX_HOME $_compilerspec -D_codegen=$_codegen .. > ../cmake.out
   make install > ../make.out
 popd
