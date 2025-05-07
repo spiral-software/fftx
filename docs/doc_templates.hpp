d4826951d7263134c1477792990efdfa3a53dac6
Patrick Broderick <patrick.broderick@gmail.com>
2025-05-07 13:50:03 -0400

    Force documentation of templated structs

---
+++ docs/doc_templates.hpp
@@ -0,0 +1,20 @@
+//  Copyright (c) 2018-2025, Carnegie Mellon University
+//  See LICENSE for details
+//
+// doc_templates.hpp
+// This file exists only to help Doxygen generate documentation
+// for FFTX template classes that would otherwise be skipped.
+
+#pragma once
+
+#include "fftx.hpp"
+
+namespace fftx {
+
+    // Force template instantiations for documentation
+    template struct point_t<3>;
+    template struct box_t<3>;
+    template struct array_t<3, double>;
+    template struct global_ptr<3, double>;
+
+} // namespace fftx
