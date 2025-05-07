9fc0aa586d4e6bac2da9e1a2e31462f6a93545fa
Patrick Broderick <patrick.broderick@gmail.com>
2025-05-07 14:00:25 -0400

    Force documentation of templated structs

--- src/include/doc_templates.hpp
+++ src/include/doc_templates.hpp
@@ -1,9 +1,10 @@
 //  Copyright (c) 2018-2025, Carnegie Mellon University
 //  See LICENSE for details
 //
-// doc_templates.hpp
-// This file exists only to help Doxygen generate documentation
-// for FFTX template classes that would otherwise be skipped.
+//  doc_templates.hpp
+//  This file exists only to help Doxygen generate documentation
+//  for FFTX template classes that would otherwise be skipped.
+//  It is never used or compiled into code.
 
 #pragma once
 
