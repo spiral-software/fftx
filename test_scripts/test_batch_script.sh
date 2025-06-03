6ee1b248b4d7c62b27da1b53e168bd464cace449
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2025-06-03 13:29:07 -0700

    Specify that executables are in $FFTX_HOME/bin, not just bin.

--- test_scripts/test_batch_script.sh
+++ test_scripts/test_batch_script.sh
@@ -25,14 +25,14 @@ N=256
 B=64
 NxB=$N"x"$B
 
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1ddft -s $NxB -i 5 -r 0x0"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1ddft -s $NxB -i 5 -r 0x1"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1ddft -s $NxB -i 5 -r 1x0"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1ddft -s $NxB -i 5 -r 1x1"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1dprdft -s $NxB -i 5 -r 0x0"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1dprdft -s $NxB -i 5 -r 0x1"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1dprdft -s $NxB -i 5 -r 1x0"
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testbatch1dprdft -s $NxB -i 5 -r 1x1"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1ddft -s $NxB -i 5 -r 0x0"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1ddft -s $NxB -i 5 -r 0x1"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1ddft -s $NxB -i 5 -r 1x0"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1ddft -s $NxB -i 5 -r 1x1"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1dprdft -s $NxB -i 5 -r 0x0"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1dprdft -s $NxB -i 5 -r 0x1"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1dprdft -s $NxB -i 5 -r 1x0"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testbatch1dprdft -s $NxB -i 5 -r 1x1"
 
 ### dimensions of distributed FFT
 M=128
@@ -40,16 +40,16 @@ N=128
 K=128
 MxNxK=$M"x"$N"x"$K
 
-time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 bin/testmddft -s $MxNxK -i 5"
+time_test "srun --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus=1 $FFTX_HOME/bin/testmddft -s $MxNxK -i 5"
 
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/fortran_main -s $MxNxK -i 5"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/fortran_main -s $MxNxK -i 5"
 
 M=64
 N=64
 K=64
 MxNxK=$M"x"$N"x"$K
 
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/fortran_main -s $MxNxK -i 5"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/fortran_main -s $MxNxK -i 5"
 
 batch=1
 
@@ -68,15 +68,15 @@ real=0
 trials=3
 check=2
 
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $fwd $complex $trials $check"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $inv $complex $trials $check"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $fwd $real    $trials $check"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $inv $real    $trials $check"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $fwd $complex $trials $check"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $inv $complex $trials $check"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $fwd $real    $trials $check"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_1D $M $N $K $batch $noembed $inv $real    $trials $check"
 
 rows=2
 cols=2
 
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $fwd $complex"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $inv $complex"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $fwd $real"
-time_test "srun --nodes=1 --ntasks=4 --gpus=4 bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $inv $real"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $fwd $complex"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $inv $complex"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $fwd $real"
+time_test "srun --nodes=1 --ntasks=4 --gpus=4 $FFTX_HOME/bin/test3DDFT_mpi_2D $M $N $K $batch $rows $cols $noembed $inv $real"
